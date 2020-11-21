9749209f4ebfa68492d9a6a06158561823520c0a
beer2002 <uvarkin77@gmail.com>
2020-11-21 17:52:20 +0300

    Version 1.0

--- src/index.js
+++ src/index.js
@@ -1,17 +1,112 @@
-import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Item extends Component{
+    render(){
+        const key = this.props.key;
+        //const children_done = this.props.children_done;
+        let className;
+
+        if(this.props.done){
+            className = 'done';
+        }
+
+        return(
+            <li className="list-group-item">
+                <input type='checkbox' className='business' onChange={() => this.props.onChange()}/>
+                <p className={className}>{this.props.text} </p>
+            </li>
+        );
+    }
+}
+
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            num_children: 0,
+            children: [],
+            children_done: [],
+            children_text: [],
+            input_value: '',
+        }
+    }
+
+    upgradeInputValue(event){
+        this.setState({
+            input_value: event.target.value,
+        });
+    }
+
+    done(key){
+        const child_done = this.state.children_done;
+        child_done[key] = !child_done[key];
+
+        this.setState(state =>{
+            return{
+                children_done: child_done,
+            }
+        })
+    }
+
+    add_item = (event) => {
+        if(event.key !== "Enter"){
+            return;
+        }
+
+        this.setState(state => {
+            const key = this.state.num_children;
+            const list = state.children.concat(<Item 
+                                                key={key}
+                                                children_done={this.state.children_done}
+                                                text={this.state.input_value} 
+                                                onChange={() => this.done(key)}/>);
+            const list_done = state.children_done.concat(false);
+            const list_text = state.children_text.concat(this.state.input_value);
+            return {
+                //children: list,
+                children_done: list_done,
+                children_text: list_text,
+                num_children: this.state.num_children + 1,
+                input_value: '',
+            }
+        });
+    }
+
+    renderList(){
+        const list = [];
+        for(let i = 0; i < this.state.num_children; i++){
+            list.push(<Item
+                        key={i}
+                        text={this.state.children_text[i]}
+                        done={this.state.children_done[i]}
+                        onChange={() => this.done(i)}/>);
+        }
+
+        return list;
+    }
+
+    render(){
+        let children = this.state.children;
+
+        return(
+            <div className='main-container'>
+                <input 
+                    type="text" className="form-control" placeholder="Что нужно сделать?"
+                    value={this.state.input_value} 
+                    onKeyPress={this.add_item}
+                    onChange={event => this.upgradeInputValue(event)}
+                />
+                {this.renderList()}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
