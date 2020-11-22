3d8fabbaff12a46b52f38eee0aaedb2e498b9027
beer2002 <uvarkin77@gmail.com>
2020-11-22 12:49:31 +0300

    Version 1.0.1

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './main.scss';
 
 
 class Item extends Component{
     render(){
-        const key = this.props.key;
+        const key = this.props.key; 
         //const children_done = this.props.children_done;
         let className;
 
@@ -19,8 +20,17 @@ class Item extends Component{
 
         return(
             <li className="list-group-item">
+                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-justify drag" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M2 12.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
+                </svg>
+
                 <input type='checkbox' className='business' onChange={() => this.props.onChange()}/>
                 <p className={className}>{this.props.text} </p>
+
+                <div class="close-container">
+                    <div class="leftright"></div>
+                    <div class="rightleft"></div>
+                </div>
             </li>
         );
     }
